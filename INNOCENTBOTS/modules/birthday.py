e132b5ce7e5b12d1ff951f34ce174064ef62b60c
nakul hackerji <155868717+nakuldkdhacker0026@users.noreply.github.com>
2024-06-01 17:02:33 +0530

    Update birthday.py

--- INNOCENTBOTS/modules/birthday.py
+++ INNOCENTBOTS/modules/birthday.py
@@ -30,11 +30,11 @@ async def hraid(e):
 
         try:
             if uid in Innotron:
-                await e.reply("ɴᴏᴘᴇ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴀᴜᴛʜᴏʀ ʙᴏᴛ ᴏᴡɴᴇʀ🍷 ")
+                await e.reply("APNE DADA KO GALI DEGA YAHI SANSKAR HAI TERE 💘✨ ")
             elif uid == OWNER_ID:
-                await e.reply("ɴᴏᴘᴇ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🤖")
+                await e.reply("KYA BE APNE BAAP KO GALI DEGA ISLIYE PEDA KIYA TEREKO. 🌿✨")
             elif uid in SUDO_USERS:
-                await e.reply("ɴᴏᴘᴇ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ꜱᴜᴅᴏ ᴜꜱᴇʀ 🫂")
+                await e.reply("ISKO GALI DEGA JISNE TERI GAAND MAARI. 💥⚡.")
             else:
                 first_name = entity.first_name
                 counter = int(xraid[1])
