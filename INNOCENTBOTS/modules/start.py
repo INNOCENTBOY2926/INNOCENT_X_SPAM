d5f32540366f7a5200fe204f77f45eaca5eb9725
nakul hackerji <155868717+nakuldkdhacker0026@users.noreply.github.com>
2024-06-01 16:44:25 +0530

    Create start.py

---
+++ INNOCENTBOTS/modules/start.py
@@ -0,0 +1,39 @@
+from telethon import __version__, events, Button
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+START_BUTTON = [
+    [
+        Button.url(" 𝐌ᴜsɪᴄ ", "https://t.me/RUHI_X_MUSICBOT"),
+        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/its_innnocent_boy_8202")
+    ],
+    [
+        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/THE_FUCKING_BOT_2926"),
+        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/THE_FUCKER_BOTS_2926")
+    ],
+]
+
+
+@X1.on(events.NewMessage(pattern="/start"))
+@X2.on(events.NewMessage(pattern="/start"))
+@X3.on(events.NewMessage(pattern="/start"))
+@X4.on(events.NewMessage(pattern="/start"))
+@X5.on(events.NewMessage(pattern="/start"))
+@X6.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X8.on(events.NewMessage(pattern="/start"))
+@X9.on(events.NewMessage(pattern="/start"))
+@X10.on(events.NewMessage(pattern="/start"))
+async def start(event):
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        bot_name = AltBot.first_name
+        bot_id = AltBot.id
+        TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
+        await event.client.send_file(
+            event.chat_id,
+            "https://telegra.ph/file/b91ca77edaed85115007f.jpg",
+            caption=TEXT,
+            buttons=START_BUTTON
+        )
