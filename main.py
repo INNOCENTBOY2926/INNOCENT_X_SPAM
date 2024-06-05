7df365dfeda74da4399e75d20e3ed07c17f8d29a
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-06-05 20:47:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\INNOCENTBOTS Deployed Successfully!")
+print("INNOCENTBOTS Deployed Successfully!")
 
 
 async def main():
